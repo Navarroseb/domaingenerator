dd74353eedae75b3aa4c7c8d3dd6c2b5fcf096d3
Navarroseb <sebastnavarro@gmail.com>
2022-02-01 15:05:07 +0000

    finalizado

--- src/app.js
+++ src/app.js
@@ -2,14 +2,23 @@
 import "bootstrap";
 import "./style.css";
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
+/* import "./assets/img/rigo-baby.jpg";
+import "./assets/img/4geeks.ico"; */
 
+let pronoun = ["the", "our"];
+let adj = ["greatt", "big", "small", "huge"];
+let noun = ["jogger", "racoon", "dancer", "killer"];
+let diffDomain = [".com", ".net", ".org", ".cl"];
 window.onload = function() {
-  let pronoun = ["the", "our"];
-  let adj = ["great", "big"];
-  let noun = ["jogger", "racoon"];
-
-  //write your code here
-  console.log("Any string");
+  let newDomain;
+  for (let itemP of pronoun) {
+    for (let itemA of adj) {
+      for (let itemN of noun) {
+        for (let itemD of diffDomain) {
+          newDomain = itemP + itemA + itemN + itemD;
+          console.log(newDomain);
+        }
+      }
+    }
+  }
 };
